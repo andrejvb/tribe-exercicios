4144dea67538e2573812b33f71da71e70d9cdd05
andrejvb <andre.jvb@gmail.com>
2022-09-21 22:52:27 -0300

    exercicio 3 feito com a função switch

--- javascript/secao-04-introducao-javascript/dia-01-introducao-javascript/script.js
+++ javascript/secao-04-introducao-javascript/dia-01-introducao-javascript/script.js
@@ -15,20 +15,66 @@
 
 
 
-const chessPiece = "Peão"
+// const chessPiece = "Peão"
 
-switch (chessPiece.toLocaleLowerCase()) {
-    case "rei": console.log('Apenas uma em qualquer da direções');
+// switch (chessPiece.toLocaleLowerCase()) {
+//     case "rei": console.log('Apenas uma em qualquer da direções');
+//         break;
+//     case "bisbo": console.log('diagonal');
+//         break;
+//     case "rainha": console.log('Diagonal, horizontal e vertical');
+//         break;
+//     case "peão": console.log('Apenas uma casa para frente, no primeiro movimento podem ser duas casas');
+//         break;
+//     case "torre": console.log('Torre -> Horizontal e vertical');
+//         break;
+//     case "cavalo": console.log('Cavalo -> "L" pode pular sobre as peças');
+//         break;
+//     default: console.log('Peça Invalida!');
+
+
+
+// Utilize if/else para fazer um programa que converta uma nota dada em porcentagem (de 0 a 100) em 
+// conceitos de A a F. Siga essas regras:
+// // Porcentagem >= 90 -> A
+
+// Porcentagem >= 80 -> B
+
+// Porcentagem >= 70 -> C
+
+// Porcentagem >= 60 -> D
+
+// Porcentagem >= 50 -> E
+
+// Porcentagem < 50 -> F
+
+// O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+
+
+
+
+
+
+
+const nota = 60;
+
+
+switch (nota) {
+    case "nota < 0 | nota > 100": console.log('Erro, nota incorreta!');
         break;
-    case "bisbo": console.log('diagonal');
+    case "nota <= 90": console.log('a');
         break;
-    case "rainha": console.log('Diagonal, horizontal e vertical');
+    case "nota >= 80": console.log('b');
         break;
-    case "peão": console.log('Apenas uma casa para frente, no primeiro movimento podem ser duas casas');
+    case "nota >= 70": console.log('c');
         break;
-    case "torre": console.log('Torre -> Horizontal e vertical');
+    case "nota >= 60": console.log('d');
         break;
-    case "cavalo": console.log('Cavalo -> "L" pode pular sobre as peças');
-        break;
-    default: console.log('Peça Invalida!');
+    case "nota >= 50": console.log('e');
+
+    default: console.log('f')
+    break;
+    
 }
+
