5c029f89535e0d5a0a820aaf1f1f50eae107b032
andrejvb <andre.jvb@gmail.com>
2022-09-21 21:06:01 -0300

    exercicio da soma do angulos

---
+++ javascript/secao-04-introducao-javascript/dia-01-introducao-javascript/script.js
@@ -0,0 +1,13 @@
+const anguloA = 65;
+
+const anguloB = 100;
+
+const anguloC = 15;
+
+let somaAngulos = anguloA + anguloB + anguloC;
+
+let todosAngPositivos = anguloA > 0 && anguloB > 0 && anguloC > 0;
+
+if (todosAngPositivos === true && somaAngulos === 180) 
+    {console.log(true);
+    } else { console.log(false);}
