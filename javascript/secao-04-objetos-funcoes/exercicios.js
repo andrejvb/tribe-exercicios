b048747280a43922998bbd719c2d3aacd7414ca5
andrejvb <andre.jvb@gmail.com>
2022-09-26 20:39:40 -0300

    Analisa diferenças com o gabarito

--- javascript/secao-04-objetos-funcoes/exercicios.js
+++ javascript/secao-04-objetos-funcoes/exercicios.js
@@ -1,23 +1,38 @@
-// Exercicio 01
+// // Exercicio 01
 
-let info = [{personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-    recorrente: true
-},
-  {
-    personagem:'Tio Patinhas',
-    origem:'Christmas on Bear Mountain',
-    nota:'Dell’s Four Color Comics #178',
-    recorrente: true
-}]
+// let info = [{personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: true
+// },
+//   {
+//     personagem:'Tio Patinhas',
+//     origem:'Christmas on Bear Mountain',
+//     nota:'Dell’s Four Color Comics #178',
+//     recorrente: true
+// }]
 
 
-for(let key in info){
-    console.log(info[0].personagem + ' e ' + info[1].personagem);
-    console.log(info[0].origem + ' e ' + info[1].origem);
-    console.log(info[0].nota + ', ' + info[1].nota);
-    if (info[0].recorrente && info[1].recorrente == true) {
-        console.log('Ambos Recorentes');
-    }
-}
+// for(let key in info){
+//     console.log(info[0].personagem + ' e ' + info[1].personagem);
+//     console.log(info[0].origem + ' e ' + info[1].origem);
+//     console.log(info[0].nota + ', ' + info[1].nota);
+//     if (info[0].recorrente && info[1].recorrente == true) {
+//         console.log('Ambos Recorentes');
+//     }
+// }
+
+/////Exercicio 02
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
