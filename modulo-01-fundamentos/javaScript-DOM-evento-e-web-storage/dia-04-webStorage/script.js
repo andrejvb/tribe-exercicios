df66b2603fd765cf125b2ed94249110da25159f7
andrejvb <andre.jvb@gmail.com>
2022-10-03 22:10:41 -0300

    primeiro exercicio

---
+++ modulo-01-fundamentos/javaScript-DOM-evento-e-web-storage/dia-04-webStorage/script.js
@@ -0,0 +1,34 @@
+window.onload = function(){
+    let select = document.querySelector('select');
+    select.addEventListener ('change', function(){
+        let selected = select.selectedOptions[0];
+        document.body.style.backgroundColor = selected.value;
+    
+    })
+}
+
+// variaveis globais
+
+
+
+
+
+
+
+
+
+// funções
+
+
+
+
+
+
+
+
+
+
+
+
+// chamando funções
+
