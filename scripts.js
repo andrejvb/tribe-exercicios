c23996a106f20732d8355f57d2876f1cab35ab1e
andrejvb <andre.jvb@gmail.com>
2022-10-24 20:02:38 -0300

    finaliza primeiro exercicio

--- scripts.js
+++ scripts.js
@@ -1,8 +1,15 @@
-const newEmployees = () => {
+const newEmployees = (callBack) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callBack('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callBack('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callBack('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  const newEmployeeGenerator = (fullName) => {
+    const email = fullName.toLowerCase().split(' ').join('_');
+    return {fullName, email:`${email}@trybe.com`};
+  }
+
+console.log(newEmployees(newEmployeeGenerator));
