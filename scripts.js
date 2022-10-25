5ec0670873648aced2229160731ac8b2a8cdfe7d
andrejvb <andre.jvb@gmail.com>
2022-10-25 18:40:01 -0300

    exercicio bonus

--- scripts.js
+++ scripts.js
@@ -43,3 +43,56 @@ const countPoints = (rightAnswers, studentAnswers, callBack) =>{
 }
 
 console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
+
+const mage = {
+    healthPoints: 130,
+    intelligence: 45,
+    mana: 125,
+    damage: undefined,
+  };
+  
+  const warrior = {
+    healthPoints: 200,
+    strength: 30,
+    weaponDmg: 2,
+    damage: undefined,
+  };
+  
+  const dragon = {
+    healthPoints: 350,
+    strength: 50,
+    damage: undefined,
+  };
+  
+  const battleMembers = { mage, warrior, dragon };
+  
+  const dragonAttack = (dragon) => {
+    const minDmg = 15;
+    const maxDmg = Math.floor(Math.random() * dragon.strength)
+    const dragonDmg = maxDmg > minDmg ? maxDmg : minDmg
+    return dragonDmg
+};
+
+// console.log(dragonAttack(dragon));
+
+const warriorAttack = (warrior) => {
+    const minDmg = warrior.strength;
+    const maxDmg = minDmg * warrior.weaponDmg;
+    const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg)
+    return warriorDamage
+}
+
+// console.log(warriorAttack(warrior));
+
+const mageAttack = (mage) => {
+    const mageMana = mage.mana;
+    const minDmg = mage.intelligence;
+    const maxDmg = minDmg * 2;
+    const turnStats = {
+        manaSpent: 0,
+        damageDealt: 'Not enough mana...',
+      };
+      return turnStats;
+}
+
+
